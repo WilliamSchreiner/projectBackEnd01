118ea1def70e12de69935447813f5cf451b1e6e3
William Gabriel Schreiner <williamg.schreiner@gmail.com>
2023-03-10 22:50:13 +0000

    alteração aula10/03

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,20 @@
 export class Database {
-#database = {};
+  #database = {};
 
   select(table:string):object {
-    const data = this.database[table] ?? []
+    const data = this.#database[table] ?? []
     // ?? = um operador que verifica se o estado da condição esta NULL ou UNDER, se esta ele NULL ele alocar o espaço para um dado ja programado.
     return data
   }
 
   insert(table, data:object):object {//espera uma objeto
 
-    if(Array.isArray(this.database[table])) {
+    if(Array.isArray(this.#database[table])) {
       // add o dado
-      this.database[table].push(data)
+      this.#database[table].push(data)
     } else {
       // altera um dado ja existente
-      this.database[table] = [data]
+      this.#database[table] = [data]
     }
 
     return data //retorna uma objeto
