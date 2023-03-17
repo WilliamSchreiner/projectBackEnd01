7bab25ff63c13a7e14bc94bd58a2d6ca66afe018
WilliamSCH <williamg.schreiner@gmail.com>
2023-03-17 20:23:54 -0300

    Alteração no metodo select e delete

--- src/database.ts
+++ src/database.ts
@@ -16,16 +16,21 @@ export class Database {
   }
 
   //catch = quando a promesa nao é comprida ele trata com outro callback
-  //parse = converte o objeto escrito para objeto 
+  //parse = converte o objeto escrito para objeto
   //Stringing = converte o objeto  para objeto escrito
 
   #persist(){
     fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2))
   }
 
-  select(table:string):object {
-    const data = this.#database[table] ?? []
+  select(table:string, id?: string):object {
+    let data = this.#database[table] ?? []
     // ?? = um operador que verifica se o estado da condição esta NULL ou UNDER, se esta ele NULL ele alocar o espaço para um dado ja programado.
+
+    if(id){ data = data.find((row:any) => {
+      return row.id === id;
+    })};
+
     return data
   }
 
@@ -48,13 +53,13 @@ export class Database {
 
 
     //procura de id
-    const rowIndex  = this.#database[table].findIndex((row) => row.id === id)
+    const rowIndex  = this.#database[table].findIndex((row:any) => row.id === id)
 
     //validando o id e delete
     if(rowIndex > -1){
       this.#database[table].slice(rowIndex, 1);
       this.#persist();
-      
+
     }
   }
 
